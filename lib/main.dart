ebca999c5bda9523afdc0491bb09a53d3d2710b0
paulovsnts <paulovsnts.dev@gmail.com>
2026-07-24 20:27:47 -0300

    chore: adiciona arquivos de documentação interna no gitignore e atualiza main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'firebase_options.dart'; 
+import 'firebase_options.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'providers/timer_provider.dart';
 import 'screens/pomodoro_screen.dart';
